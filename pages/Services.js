2cd3c9668911d3768387050365dab0f17d6b4ef6
vampire32 <moizabdul320@gmail.com>
2022-11-12 18:59:46 +0500

    changes

--- pages/Services.js
+++ pages/Services.js
@@ -21,7 +21,7 @@ const Services = () => {
   return (
 		<div>
             <Head>
-                <title>Services</title>
+                <title>Products</title>
             </Head>
 			<section
 				className="u-align-center u-clearfix u-gradient u-section1"
@@ -30,7 +30,7 @@ const Services = () => {
 			>
 				<Navbar />
 				<LandingPage
-					Name="Services"
+					Name="Products"
 					desc="The company has staff in the professional, technical & administration categories. We are recognized as a supplier of Information & Communication Technology equipment, Softwares and associated products. "
 				/>
 			</section>
@@ -162,66 +162,7 @@ const Services = () => {
 					</div>
 				</div>
 			</div>
-			<section
-				className="u-clearfix u-color-scheme-u10 u-color-style-multicolor-1 u-grey-90 u__section_111"
-				id="sec-d871"
-				data-aos="zoom-in"
-			>
-				<div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
-					<div className="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
-						<div className="u-layout">
-							<div className="u-layout-row">
-								<div className="u-align-center-sm u-align-center-xs u-align-justify-lg u-align-justify-md u-align-justify-xl u-container-style u-layout-cell u-left-cell u-size-42 u-layout-cell-1">
-									<div className="u-container-layout u-valign-middle u-container-layout-1">
-										<h3 className="u-custom-font u-font-ubuntu u-text u-text-1">
-											Are You Interested Call Us Now!
-											<br />
-											<span className="u-text-palette-1-base">
-												<b>
-													<span className="u-text-palette-3-base">
-														+92-21-34390018
-													</span>
-												</b>
-											</span>
-											<span className="u-text-palette-3-base"></span>
-											<br />
-											<br />
-											<span style={{ fontSize: "1.5rem" }}>
-												All you have to do is phone or email us for an
-												appointment, and our assessment specialist will contact
-												you. Don’t waste any more time on various platforms;
-												call us right now and let&nbsp; acquire it from us.
-											</span>
-											<br />
-										</h3>
-									</div>
-								</div>
-								<div className="u-align-left u-container-style u-layout-cell u-right-cell u-size-18 u-layout-cell-2">
-									<div className="u-container-layout u-container-layout-2">
-										<a
-											href="tel:+92-21-34390018"
-											className="u-border-none u-btn u-btn-round u-button-style u-hover-white u-palette-3-base u-radius-50 u-btn-1"
-										>
-											<span className="u-icon">
-												<svg
-													className="u-svg-content"
-													viewBox="0 0 513.64 513.64"
-													x="0px"
-													y="0px"
-													style={{ width: "1em", height: "1em" }}
-												>
-													<path d="M499.66,376.96l-71.68-71.68c-25.6-25.6-69.12-15.359-79.36,17.92c-7.68,23.041-33.28,35.841-56.32,30.72 c-51.2-12.8-120.32-79.36-133.12-133.12c-7.68-23.041,7.68-48.641,30.72-56.32c33.28-10.24,43.52-53.76,17.92-79.36l-71.68-71.68 c-20.48-17.92-51.2-17.92-69.12,0l-48.64,48.64c-48.64,51.2,5.12,186.88,125.44,307.2c120.32,120.32,256,176.641,307.2,125.44 l48.64-48.64C517.581,425.6,517.581,394.88,499.66,376.96z"></path>
-												</svg>
-											</span>
-											&nbsp;Call now
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+			
             <Footer/>
 		</div>
 	);
